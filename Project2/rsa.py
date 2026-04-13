e840646088af94a65b36bf5c5eec6792d93ddced
SamuelJ11 <sjab225@uky.edu>
2026-04-13 05:00:24 +0000

    Started Python Version (I gave up on C++ . . . for now)

---
+++ Project2/rsa.py
@@ -0,0 +1,37 @@
+import string, sys, operator
+from pathlib import Path
+
+class RSA:
+     
+    def __init__(self):
+        self.p = 3
+        self.q = 5
+        self.d = 7
+
+    def computeN(p, q):
+
+        pass
+
+    def generate_pubkey(n, e):
+
+        pass
+
+    def generate_privkey(n, d):
+
+        pass
+
+
+    def encrypt(message, keyname):
+
+        pass
+
+    def decrypt(ciphertext, keyname):
+
+        pass
+
+
+def main():
+
+    pass
+
+main()
---
+++ Project2/utils.py
@@ -0,0 +1,22 @@
+import string, sys, operator
+from pathlib import Path
+
+def generate_randint():
+
+    pass
+
+def  modular_exponentiation(base, exponent, modulus):
+
+    pass
+
+def is_prime(number):
+
+    pass
+
+def export_keys(keyname):
+
+    pass
+
+def compute_D(e, p, q):
+
+    pass
