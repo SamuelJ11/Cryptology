04c3c1dfa794a316a32e0ed253c0fecd86102a47
SamuelJ11 <sjab225@uky.edu>
2026-04-17 03:03:32 +0000

    nothing new here . . .

--- Project2/main.py
+++ Project2/main.py
@@ -3,9 +3,9 @@ from utils import *
 
 import random
 
-for _ in range(10):
-    is_prime(15)
 
-# print(modular_exponentiation(4, 14, 15))
+print(modular_exponentiation(513, 31901, 65537))
+
+    
 
 # print(modular_exponentiation(11, 14, 15))
