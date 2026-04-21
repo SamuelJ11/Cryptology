009bd11e9944699bc6520a701ffd3af8a6c5e6b0
SamuelJ11 <sjab225@uky.edu>
2026-04-21 19:46:19 +0000

    Nothing new here ...

--- Project2/main.py
+++ Project2/main.py
@@ -2,14 +2,13 @@ from rsa import RSA
 from utils import *
 import time
 
-for _ in range(10):
-    x = random.randint(4, 64)
-    is_prime(x)
-    time.sleep(2)
+for _ in range(100):
+    is_prime(17)
     
+
 '''
 for _ in range(10):
-    sequence = blumblumslub()
+    sequence = blumblumshub()
     print(sequence)    
     time.sleep(3)
     
