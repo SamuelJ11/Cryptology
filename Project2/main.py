92456647e0a97ea304ccdcd9f36b312023e10292
SamuelJ11 <sjab225@uky.edu>
2026-04-21 11:29:14 +0000

    updated testing parameters

--- Project2/main.py
+++ Project2/main.py
@@ -2,8 +2,15 @@ from rsa import RSA
 from utils import *
 import time
 
- 
+for _ in range(10):
+    x = random.randint(4, 64)
+    is_prime(x)
+    time.sleep(2)
+    
+'''
 for _ in range(10):
     sequence = blumblumslub()
     print(sequence)    
     time.sleep(3)
+    
+'''
