fa7a7af8908e11ebde2432ec5386c51bad00f898
SamuelJ11 <sjab225@uky.edu>
2026-04-21 08:39:47 +0000

    changed sleep time

--- Project2/main.py
+++ Project2/main.py
@@ -2,12 +2,6 @@ from rsa import RSA
 from utils import *
 import time
 
-for _ in range(10):
-    x = random.randint(1, 32)
-    xbin = to_binary(x)
-    print(f"x = {x} = {xbin}")
-
-print()
     
 for _ in range(10):
     sequence = blumblumslub()
