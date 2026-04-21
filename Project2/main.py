5b5168c0762f4836e4e954a5d7d95018ba58c626
SamuelJ11 <sjab225@uky.edu>
2026-04-21 07:40:52 +0000

    added more rigorous testing

--- Project2/main.py
+++ Project2/main.py
@@ -1,5 +1,16 @@
 from rsa import RSA
 from utils import *
+import time
 
-sequence = blumblumslub()
-print(sequence)
+for _ in range(10):
+    x = random.randint(1, 32)
+    xbin = to_binary(x)
+    print(f"x = {x} = {xbin}")
+
+print()
+    
+for _ in range(10):
+    sequence = blumblumslub()
+    print(sequence)    
+    time.sleep(5)
+    
