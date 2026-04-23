dbc5834897c5349d35c8cd5572eafaccfc1113b3
SamuelJ11 <sjab225@uky.edu>
2026-04-23 07:24:22 +0000

    more testing

--- Project2/main.py
+++ Project2/main.py
@@ -2,14 +2,23 @@ from rsa import RSA
 from utils import *
 import time
 
-for _ in range(100):
-    is_prime(17)
-    
-
-'''
+   
 for _ in range(10):
     sequence = blumblumshub()
-    print(sequence)    
-    time.sleep(3)
+    print(f"Taking the LSB of each number in Blum sequence yeilds: {sequence}\n")    
+    time.sleep(2)
+
+
+for _ in range(20):
+    x = random.randint(1, 20)
+    y = random.randint(1, 20)
+    result = gcd_recursive(x, y)
     
-'''
+    print(f"The GCD of {x} and {y} is {result}")
+    time.sleep(2)
+    
+
+for _ in range(10):
+    x = random.randint(100, 1000)
+    is_prime(x)
+    time.sleep(2)
