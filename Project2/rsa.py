0d9cd44a8ba897eb798b412204731f93cd7a22ee
SamuelJ11 <sjab225@uky.edu>
2026-04-24 05:39:32 +0000

    oh yeah ... we're cooking now

--- Project2/rsa.py
+++ Project2/rsa.py
@@ -1,23 +1,36 @@
-import string, utils
+import string
+import utils
 from pathlib import Path
 
 class RSA:
      
-    def __init__(self):
-        self.p = 3
-        self.q = 5
-        self.d = 7
+    def __init__(self, p, q):
+        self.p = p
+        self.q = q
+        self.d = None
+        self.e = None
+        self.n = None
 
     def computeN(self):
-        pass
-    
-    def compute_E(self):
         
-        pass
+        self.n = self.p * self.q
+        
+    def computeE(self):
+        
+        if (self.n == None):
+            print("Warning: modulus not defined, unable to compute the encyrption exponent")
+            return
+        
+        encryption_exp = utils.generate_prime()        
+        modulus = utils.totient(self.n, self.p, self.q)
+        
+        while (utils.gcd_recursive(encryption_exp, modulus) != 1):
+            encryption_exp = utils.generate_prime()
+        
+        self.e = encryption_exp
     
-    def compute_D(self, e):
-
-        pass
+    def computeD(self, e, n):
+        self.d = utils.mod_inverse(e, utils.totient(n))
 
     def generate_pubkey(self, n, e):
         
